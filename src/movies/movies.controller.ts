8ad3b1c03f11ee04f4f0f30e9d102ce8fcd9e770
vividsunmi <carpedm43@gmail.com>
2021-03-13 02:49:46 +0900

    modify movie controller and service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,30 +1,43 @@
-import { Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
-
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Delete,
+    Patch,
+    Body,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+
     @Get()
-    getAll() {
-        return 'This will return all movies';
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
 
-    @Get('/:id')
-    getOne(@Param('id') movieId: number) {
-        return `This will return one movie with the id: ${movieId}`;
+    @Get(':id')
+    getOne(@Param('id') movieId: number): Movie {
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create() {
-        return 'This will create a movie';
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
     }
 
-    @Delete('/:id')
+    @Delete(':id')
     remove(@Param('id') movieId: number) {
-        return `This will delete a movie with the id: ${movieId}`;
+        return this.moviesService.deleteOne(movieId);
     }
 
-    @Patch('/:id')
-    patch(@Param('id') movieId: number) {
-        return `This will patch a movie with the id: ${movieId}`;
+    @Patch(':id')
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(movieId, updateData);
     }
 }
